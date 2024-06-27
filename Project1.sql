4f611a9328115ea0fe95e5b6e03df34d3e0441f5
anusha anusha <anusham.xworkz58@gmail.com>
2024-06-27 21:24:36 +0530

    26-06-24 TASK

---
+++ Project1.sql
@@ -0,0 +1,6 @@
+CREATE DATABASE Project1;
+CREATE Table  Airport_detail(id int,airport_name varchar(20),arrival int,departure int,price int)
+use airport;
+
+CREATE Table Cricket_teamss(id int,team_name varchar(20),no_of_players int, captain varchar(10),coach varchar(20))
+
---
+++ Projects.sql
@@ -0,0 +1,6 @@
+CREATE DATABASE Projects;
+CREATE Table  Airport_details(id int,airport_name varchar(20),arrival int,departure int,price int)
+use airport;
+
+CREATE Table Cricket_teamsss(id int,team_name varchar(20),no_of_players int, captain varchar(10),coach varchar(20))
+
