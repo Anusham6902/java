64375e38098981daca34d79b51f2ea043aa9be70
anusha anusha <anusham.xworkz58@gmail.com>
2024-06-27 20:49:44 +0530

    26-06-24 TASK

---
+++ Cube_Number.java
@@ -0,0 +1,8 @@
+public class Cube_Number {
+     
+    public static void main(String[] args) {
+        int number = 5;
+		int cube = number * number * number*number * number * number;
+		System.out.println("The cube of " + number + " is: " + cube);
+    }
+}
