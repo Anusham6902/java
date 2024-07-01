49519b8b355bdc42ad1b02fb94b03719d63a4e75
anusha anusha <anusham.xworkz58@gmail.com>
2024-07-01 12:38:09 +0530

    28-06-24 TASK

---
+++ TV_ChannelsRunner.java
@@ -0,0 +1,7 @@
+public class TV_ChannelsRunner{
+     public static void main (String []args){
+	 System.out.println("Apart from news channels,another important category of television channels are sports channels");
+	 	 System.out.println("Tata play have  13 Number of Kannada channel");
+		 TV_Channels.allchannels();
+		 }
+		 }
