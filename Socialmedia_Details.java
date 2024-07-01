e2fefb1712709fb1de932cb44bc1450b4a48b9c7
anusha anusha <anusham.xworkz58@gmail.com>
2024-07-01 12:18:54 +0530

    28-06-24 TASK

---
+++ Socialmedia_Details.java
@@ -0,0 +1,7 @@
+public class Socialmedia_Details{
+     public static void main (String []args){
+	 System.out.println("It was the amazing tool for staying connected with the people in our life");
+	 	 System.out.println("Andrew Weinreich is the father of social media");
+		 Socialmedia.platforms();
+		 }
+		 }
