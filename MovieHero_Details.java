15cf6528e5140c3e4830ea648d5d229e9acee654
anusha anusha <anusham.xworkz58@gmail.com>
2024-07-01 10:32:40 +0530

    28-06-24 TASK

---
+++ MovieHero_Details.java
@@ -0,0 +1,7 @@
+public class MovieHero_Details{
+     public static void main (String []args){
+	 System.out.println("My favorite movie hero is rocking star yash");
+	 	 System.out.println("His first movie is Rocky");
+		MovieHero.favroitemoviehero();
+		 }
+		 }
