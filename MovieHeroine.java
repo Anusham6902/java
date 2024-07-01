d6b0fb4b8bd8e42a50387860a72d6eb9a51513a8
anusha anusha <anusham.xworkz58@gmail.com>
2024-07-01 10:33:34 +0530

    28-06-24 TASK

---
+++ MovieHeroine.java
@@ -0,0 +1,5 @@
+public class MovieHeroine{
+     public static void favroitemovieheroine(){
+	 System.out.println("She won the Filmfare Award for Best Actress and the Karnataka State Film Awards.");
+	 }
+}
