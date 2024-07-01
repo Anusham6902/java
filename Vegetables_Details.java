3d3db04c52149f90a2ea14245a3b6040bdec183d
anusha anusha <anusham.xworkz58@gmail.com>
2024-07-01 12:40:49 +0530

    28-06-24 TASK

---
+++ Vegetables_Details.java
@@ -0,0 +1,7 @@
+public class Vegetables_Details{
+     public static void main (String []args){
+	 System.out.println("Vegetables provide nutrients vital for health and maintenance of our body");
+	 	 System.out.println("A vegetable is the edible portion of a plant");
+		Vegetables.aboutvegetables();
+		 }
+		 }
