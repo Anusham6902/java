03249116368f9fed270a22bccd97ac6c6d18be4f
anusha anusha <anusham.xworkz58@gmail.com>
2024-07-01 12:34:51 +0530

    28-06-24 TASK

---
+++ Songs_Details.java
@@ -0,0 +1,7 @@
+public class Songs_Details{
+     public static void main (String []args){
+	 System.out.println("There are so many kannada songs");
+	 	 System.out.println("My favourite song is drama movie song chandutiya pakkadali....");
+		 Songs.kannadasong();
+		 }
+		 }
