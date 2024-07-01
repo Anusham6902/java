6b06c652ad1f06750c24520d50582200fa5add6e
anusha anusha <anusham.xworkz58@gmail.com>
2024-07-01 12:06:04 +0530

    28-06-24 TASK

---
+++ NationalPoet_Details.java
@@ -0,0 +1,7 @@
+public class NationalPoet_Details{
+     public static void main (String []args){
+	 System.out.println("Manjeshwara Govinda Pai is the first National Poet in Karnataka");
+	 	 System.out.println("He was awarded the first Rashtrakavi title by the Madras Government");
+		 NationalPoet.kannadanationalpoets();
+		 }
+		 }
