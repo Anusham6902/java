fde94c1fc97d7551e1597dbc7a12b385d966f009
anusha anusha <anusham.xworkz58@gmail.com>
2024-07-01 10:34:24 +0530

    28-06-24 TASK

---
+++ MovieHeroine_Details.java
@@ -0,0 +1,7 @@
+public class MovieHeroine_Details{
+     public static void main (String []args){
+	 System.out.println("My favorite movie heroine is Radhika Pandit");
+	 	 System.out.println("her first movie is Moggina Manasu ");
+		MovieHeroine.favroitemovieheroine();
+		 }
+		 }
