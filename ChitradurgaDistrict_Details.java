25d006059c4df6c59db192c00a0a0120a91f7d70
anusha anusha <anusham.xworkz58@gmail.com>
2024-07-01 10:13:08 +0530

    28-06-24 TASK

---
+++ ChitradurgaDistrict_Details.java
@@ -0,0 +1,7 @@
+public class ChitradurgaDistrict_Details{
+     public static void main (String []args){
+	 System.out.println("Chitradurga is a place with historical significance which is located to the North West about 200 km from the state capital Bangalore");
+	 	 System.out.println("Chitradurga is a major tourist hub in Karnataka");
+		 ChitradurgaDistrict.aboutchitradurgadistrict();
+		 }
+		 }
