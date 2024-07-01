1fd54027c255fd97ae437b13d7b01f81da13facc
anusha anusha <anusham.xworkz58@gmail.com>
2024-07-01 12:10:48 +0530

    28-06-24 TASK

---
+++ Police_Details.java
@@ -0,0 +1,7 @@
+public class Police_Details{
+     public static void main (String []args){
+	 System.out.println("The full form of POLICE is Public Officer for Legal Investigations and Criminal Emergencies");
+	 	 System.out.println("The police are entrusted with the duty of maintaining the peace and harmony of a society");
+		 Police.aboutPolice();
+		 }
+		 }
