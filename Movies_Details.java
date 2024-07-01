e9727759008ed62650c3aed7938bb3f5a82e7a1f
anusha anusha <anusham.xworkz58@gmail.com>
2024-07-01 12:03:46 +0530

    28-06-24 TASK

---
+++ Movies_Details.java
@@ -0,0 +1,7 @@
+public class Movies_Details{
+     public static void main (String []args){
+	 System.out.println("There are so many kannada Movies");
+	 	 System.out.println("My favourite movie is MODALASALA");
+		 Movies.kannadamovie();
+		 }
+		 }
