d23ad22d1dba89a50716c35af1de340510c54d80
anusha anusha <anusham.xworkz58@gmail.com>
2024-07-01 12:14:35 +0530

    28-06-24 TASK

---
+++ Soap_Details.java
@@ -0,0 +1,7 @@
+public class Soap_Details{
+     public static void main (String []args){
+	 System.out.println("Soap kills microorganisms by disorganizing their membrane lipid bilayer and denaturing their proteins");
+	 	 System.out.println("Soap is created by mixing fats and oils with a base");
+		 Soap.aboutsoap();
+		 }
+		 }
