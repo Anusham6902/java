8e71f6708aaf05b4fa1bd34f78aa3006574486c9
anusha anusha <anusham.xworkz58@gmail.com>
2024-07-01 12:09:01 +0530

    28-06-24 TASK

---
+++ Police.java
@@ -0,0 +1,5 @@
+public class Police{
+     public static void aboutPolice(){
+	 System.out.println("Police officers arrest criminals, prevent crime, protect and help the public, and keep public order");
+	 }
+}
