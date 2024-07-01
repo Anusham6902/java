dd41f4ec42c0f8808f0b3a266c72c5e4afe895f0
anusha anusha <anusham.xworkz58@gmail.com>
2024-07-01 10:28:27 +0530

    28-06-24 TASK

---
+++ Hockey_Details.java
@@ -0,0 +1,7 @@
+public class Hockey_Details{
+     public static void main (String []args){
+	 System.out.println("Hockey is the national sport of India");
+	 	 System.out.println("The Indian Hockey Federation approved it in 1925");
+		 Hockey.abouthockey();
+		 }
+		 }
