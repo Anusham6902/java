c5a2c5bf99f85bcda724727853910ff92a781c96
anusha anusha <anusham.xworkz58@gmail.com>
2024-07-01 10:21:30 +0530

    28-06-24 TASK

---
+++ Engineering_College_Details.java
@@ -0,0 +1,7 @@
+public class Engineering_College_Details{
+     public static void main (String []args){
+		 System.out.println("Sri Jagadguru Murugarajendra Institute Of Technology Engineering College");
+		 System.out.println("Located In Chitradurga");
+		 Engineering_College.SJMIT();
+	 }
+}
