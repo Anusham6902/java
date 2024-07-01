0f50b20efa18697bf956e97be772531a58032d98
anusha anusha <anusham.xworkz58@gmail.com>
2024-07-01 10:26:35 +0530

    28-06-24 TASK

---
+++ Gmails_Details.java
@@ -0,0 +1,7 @@
+public class Gmails_Details{
+     public static void main (String []args){
+	 System.out.println("My gmail ID is valmikianusha95@gmail.com");
+	 	 System.out.println("password:12345678");
+		Gmail.aboutgmails();
+		 }
+		 }
