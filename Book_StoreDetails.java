e49d76d5676f0ff3dcde1ae8b14d88c7dc7ec7c2
anusha anusha <anusham.xworkz58@gmail.com>
2024-07-01 10:07:38 +0530

    28-06-24 TASK

---
+++ Book_StoreDetails.java
@@ -0,0 +1,7 @@
+public class Book_StoreDetails{
+     public static void main (String[]args){
+	 System.out.println("Available All Types Books");
+	 	 System.out.println("Timings at 9AM to 5PM");
+		 Book_Store.books();
+		 }
+		 }
