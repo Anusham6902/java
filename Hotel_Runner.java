e865d02c81ce699670904c27e308e23226b37779
anusha anusha <anusham.xworkz58@gmail.com>
2024-07-13 10:32:08 +0530

    11-07-24 TASK

---
+++ Hotel_Runner.java
@@ -0,0 +1,9 @@
+public class Hotel_Runner{
+     public static void main(String args[]){
+      Hotel.getHotelMenu();
+	  Hotel.getHotelName();
+	  Hotel.getHotelowner();
+	  Hotel.getOneItemPrice();
+	   Hotel.getTotalhotelWorkers();
+     }
+}	 
