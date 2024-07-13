48a56f8f40c5083bf3a61918ddb6831b929d71d2
anusha anusha <anusham.xworkz58@gmail.com>
2024-07-13 10:30:41 +0530

    11-07-24 TASK

---
+++ Hostel.java
@@ -0,0 +1,28 @@
+public class Hostel{
+     
+	 static String hostelName;
+	 static int totalNumber;
+	 static String fees;
+	 static float hostelRatings;
+	 static boolean hostelfood;
+	 
+public static void abouthostel(String hostelName){
+	System.out.println("hostelname is:"+hostelName);
+}
+public static void getinfo(int totalNumber){
+	System.out.println("total number of students:"+totalNumber);
+}
+public static void setinfo(String fees){
+	System.out.println("Entry fees:"+fees);
+}
+public static void opinion(float hostelRatings){
+	System.out.println("ratings are good:"+hostelRatings);
+}
+public static void feel(boolean hostelfood){
+	System.out.println("food is available:"+hostelfood);
+}
+
+
+
+
+}
